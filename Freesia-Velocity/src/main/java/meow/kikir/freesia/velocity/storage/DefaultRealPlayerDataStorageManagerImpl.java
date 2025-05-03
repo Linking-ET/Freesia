3a68ca5371363f0069ec74ac06b02709233be2c1
MrHua269 <mrhua269@gmail.com>
2025-05-03 18:24:14 +0800

    Save named nbt tags instead of unnamed tags
    I know it's a waste of performance but sometimes it's too hard to use a tool to edit the unnamed tags tho. So I converted it back in this commit of this fork version.And it's not too waste because the structure of the nbt of ysm is quite simple and there is closely unnecessary to consider that issue

--- Freesia-Velocity/src/main/java/meow/kikir/freesia/velocity/storage/DefaultRealPlayerDataStorageManagerImpl.java
+++ Freesia-Velocity/src/main/java/meow/kikir/freesia/velocity/storage/DefaultRealPlayerDataStorageManagerImpl.java
@@ -1,10 +1,13 @@
 package meow.kikir.freesia.velocity.storage;
 
+import com.github.retrooper.packetevents.protocol.nbt.NBTCompound;
+import com.github.retrooper.packetevents.protocol.nbt.NBTLimiter;
+import com.github.retrooper.packetevents.protocol.nbt.serializer.DefaultNBTSerializer;
+import com.github.retrooper.packetevents.protocol.nbt.serializer.NBTSerializer;
 import meow.kikir.freesia.velocity.FreesiaConstants;
 import meow.kikir.freesia.velocity.Freesia;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -20,7 +23,20 @@ public class DefaultRealPlayerDataStorageManagerImpl implements IDataStorageMana
             }
 
             try {
-                return Files.readAllBytes(targetFile.toPath());
+                // Covert to unnamed nbt
+                final byte[] data =  Files.readAllBytes(targetFile.toPath());
+                final ByteArrayInputStream inputStream = new ByteArrayInputStream(data);
+                final DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+                final DefaultNBTSerializer codec = new DefaultNBTSerializer();
+
+                final NBTCompound deserialized = (NBTCompound) codec.deserializeTag(NBTLimiter.noop(), dataInputStream, true);
+
+                final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                final DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+                codec.serializeTag(dataOutputStream, deserialized, false);
+
+                return outputStream.toByteArray();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -34,7 +50,19 @@ public class DefaultRealPlayerDataStorageManagerImpl implements IDataStorageMana
             final File targetFile = new File(FreesiaConstants.FileConstants.PLAYER_DATA_DIR, playerUUID + ".nbt");
 
             try {
-                Files.write(targetFile.toPath(), content);
+                // Covert to named nbt
+                final ByteArrayInputStream inputStream = new ByteArrayInputStream(content);
+                final DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+                final DefaultNBTSerializer codec = new DefaultNBTSerializer();
+
+                final NBTCompound deserialized = (NBTCompound) codec.deserializeTag(NBTLimiter.noop(), dataInputStream, false);
+
+                final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                final DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+                codec.serializeTag(dataOutputStream, deserialized, true);
+
+                Files.write(targetFile.toPath(), outputStream.toByteArray());
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
